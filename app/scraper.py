a61e19c1c1ca9e5590f22824ee4b179ef555994a
joeriddles <joeriddles10@gmail.com>
2021-12-16 23:04:57 -0800

    Refactor scraper to parse and then update db models

--- app/scraper.py
+++ app/scraper.py
@@ -5,7 +5,6 @@ from typing import Optional
 import bs4
 import requests
 
-from app import db
 from app.db_service import DbService
 from app.models.item import Item
 
@@ -23,21 +22,25 @@ def scrape(url: Optional[str] = None):
         logging.error(str(e))
         return
 
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    rows = soup.find_all("li", class_="result-row")
+    items: list[Item] = []
+    for row in rows:
+        item_id = row["data-pid"]
+        url = row.find("a", id=f"postid_{item_id}")["href"]
+        logging.debug(url)
+        text = str(row)
+        item = Item(
+            item_id=item_id,
+            url=url,
+            text=text,
+        )
+        items.append(item)
+
     db_service = DbService(Item)
-    with db.create_session() as session:
-        soup = bs4.BeautifulSoup(response.text, "html.parser")
-        for row in soup.find_all("li", class_="result-row"):
-            item_id = row["data-pid"]
-            url = row.find("a", id=f"postid_{item_id}")["href"]
-            logging.info(url)
-            text = str(row)
-            item = Item(
-                item_id=item_id,
-                url=url,
-                text=text,
-            )
-            with db_service:
-                db_service.add_or_update(Item.item_id == item.item_id, item)
+    with db_service:
+        for item in items:
+            db_service.add_or_update(Item.item_id == item.item_id, item)
 
 
 if __name__ == "__main__":
